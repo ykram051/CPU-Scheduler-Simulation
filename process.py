eefab61f78f36c55695fc96bac29d99f3c3f0dbc
ykram051 <wikam5050@gmail.com>
2025-04-28 19:46:08 +0100

    Process and FCFS

---
+++ process.py
@@ -0,0 +1,44 @@
+class Process:
+    """
+    Class to represent a process in the CPU scheduling simulation.
+    
+    Attributes:
+        pid (int): Process ID
+        arrival_time (int): Time at which the process arrives in the ready queue
+        burst_time (int): Total CPU time required by the process
+        priority (int): Priority of the process (lower value means higher priority)
+        remaining_time (int): Remaining CPU time required by the process
+        completion_time (int): Time at which the process completes execution
+        waiting_time (int): Total time spent by the process waiting in the ready queue
+        turnaround_time (int): Total time taken from arrival to completion
+        response_time (int): Time at which the process first gets the CPU
+        started (bool): Flag indicating whether the process has started execution
+    """
+    
+    def __init__(self, pid, arrival_time, burst_time, priority=0):
+        self.pid = pid
+        self.arrival_time = arrival_time
+        self.burst_time = burst_time
+        self.priority = priority
+        
+        # These will be calculated during scheduling
+        self.remaining_time = burst_time
+        self.completion_time = 0
+        self.waiting_time = 0
+        self.turnaround_time = 0
+        self.response_time = None  # Time when process first gets CPU
+        self.started = False
+    
+    def __str__(self):
+        return f"Process {self.pid} (Arrival: {self.arrival_time}, Burst: {self.burst_time}, Priority: {self.priority})"
+    
+    def reset(self):
+        """
+        Reset process state for a new scheduling simulation.
+        """
+        self.remaining_time = self.burst_time
+        self.completion_time = 0
+        self.waiting_time = 0
+        self.turnaround_time = 0
+        self.response_time = None
+        self.started = False
---
+++ schedulers/fcfs.py
@@ -0,0 +1,51 @@
+from . import Scheduler
+import metrics
+
+class FCFSScheduler(Scheduler):
+    def __init__(self, processes):
+        super().__init__(processes)
+        
+    def schedule(self):
+        # Sort processes by arrival time
+        self.processes.sort(key=lambda p: p.arrival_time)
+        
+        # Make a copy of processes to work with
+        remaining_processes = self.processes.copy()
+        
+        # Continue until all processes are completed
+        while remaining_processes:
+            # Get the earliest arrived process that hasn't been processed yet
+            # In FCFS, we always pick the process that arrived first
+            current_process = remaining_processes[0]
+            
+            # If the process hasn't arrived yet, advance time to its arrival
+            if current_process.arrival_time > self.current_time:
+                self.current_time = current_process.arrival_time
+                
+            # If this is the first time process is getting CPU, set start time
+            if current_process.start_time is None:
+                current_process.start_time = self.current_time
+            
+            # Execute the process completely (FCFS is non-preemptive)
+            self.execution_sequence.append({
+                'pid': current_process.pid,
+                'start': self.current_time,
+                'end': self.current_time + current_process.remaining_time
+            })
+            
+            # Update current time
+            self.current_time += current_process.remaining_time
+            
+            # Mark process as completed
+            current_process.completion_time = self.current_time
+            current_process.remaining_time = 0
+            self.completed_processes.append(current_process)
+            
+            # Remove the process from remaining processes
+            remaining_processes.remove(current_process)
+        
+        # Calculate metrics using the metrics module
+        return {
+            **metrics.calculate_fcfs_metrics(self.completed_processes, self.execution_sequence),
+            'execution_sequence': self.execution_sequence
+        }
